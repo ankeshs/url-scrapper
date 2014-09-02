6bd846f8397b032bfd5671f6748f12e59f4747e7
Ankesh Kumar Singh <ank.singh90@gmail.com>
2014-09-02 15:00:37 +0530

    Error handlers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config
 import urllib2
 import xml.etree.ElementTree as ET
 from BeautifulSoup import BeautifulSoup
+import hashlib
 
 db = Database(config.db_conf)
 db.connectDb()
@@ -10,21 +11,80 @@ db.connectDb()
 sitemaps = db.getTableData("sitemaps", ["id", "name", "url"], "flag=1", None, "priority desc")
 
 for row in sitemaps:
-    smap = urllib2.urlopen(row['url'])    
-    root = ET.fromstring(smap.read())    
+    smap = None
+    try:
+        smap = urllib2.urlopen(row['url'])    
+    except :
+        print "Failed to open " + row['name'] + " sitemap"
+        continue
+    
+    root = None
+    try:
+        root = ET.fromstring(smap.read())   
+    except :
+        print "Failed to parse " + row['name'] + " sitemap XML"
+        continue
+    
     nspace = root.tag[:root.tag.find('urlset')]    
+    fetched = 0
+    skipped = 0
+    
     for url in root:
         loc = url.find(nspace + "loc")
-        story = urllib2.urlopen(loc.text) 
-        htm = BeautifulSoup(story.read())
+        if len(db.getTableData("stories", ["id"], "flag=1" 
+            + " AND url='" + loc.text + "'")) is 0 :
+            
+            story = None
+            try:
+                story = urllib2.urlopen(loc.text) 
+            except :
+                print "Failed to Open url " + loc.text
+                continue
+            
+            try:
+                htm = BeautifulSoup(story.read())
+            except :
+                print "Failed to parse html from " + loc.text
+                continue
+            
+            permalink = htm.find("meta", property="og:url")['content']
+            
+            if permalink is None:
+                permalink = htm.find("link", rel="canonical")['href']
+            
+            if permalink is None:
+                permalink = url
+                
+            permalink_hash = abs(hash(permalink)) % (10 ** 13)
+            
+            title = htm.find("title").text
+                    
+            if len(db.getTableData("stories", ["id"], "flag=1" 
+                + " AND permalink_hash=" + str(permalink_hash)
+                + " AND permalink= '" + permalink + "'")) is 0 :
+                    db.insertTableData('stories', {'site_id' : row['id'],
+                        'permalink_hash' : permalink_hash,
+                        'permalink' :  permalink ,
+                        'title' :  title ,
+                        'url' :  loc.text })
+                    
+                    db.commitDb()
+                        
+                    fetched = fetched + 1
+                    print "Fetched " + str(fetched) + " from " + row['name']   
+            
+            else :
+                skipped = skipped + 1
+                print "Skipped " + str(skipped) + " from " + row['name']   
+            
+        db.insertTableData('sitemap_fetch', {'site_id' : row['id'], 'fetched' : fetched}) 
+        db.commitDb()
+
         
-        permalink = htm.find("meta", property="og:url")['content']
+db.commitDb()
+db.closeDb()
+            
         
-        if permalink is None:
-            permalink = htm.find("link", rel="canonical")['href']
-        
-        if permalink is None:
-            permalink = url
         
             
     
--- sqllib.py
+++ sqllib.py
@@ -9,12 +9,18 @@ class Database:
         
     def connectDb(self):
         if self.conn is None:
-            db = MySQLdb.connect(host=self.host, user=self.user, passwd=self.password, db=self.dbname)
-            self.conn = db.cursor()
+            self.db = MySQLdb.connect(host=self.host, user=self.user, passwd=self.password, db=self.dbname)
+            self.conn = self.db.cursor()
     
     def executeSql(self, query):
         self.conn.execute(query)
         return self.conn.fetchall()    
+        
+    def commitDb(self):
+        self.db.commit()
+        
+    def closeDb(self):
+        self.db.close()
     
     def getTableData(self, table, column="*", condition=None, group=None, order=None, limit=None, debug=False):
         query = "SELECT "
@@ -47,3 +53,23 @@ class Database:
         data = [{cols[i]:column for i, column in enumerate(value)} for value in result]
         
         return data
+
+    def insertTableData(self, table, data, debug=False):
+        query = "INSERT INTO " + table
+        query += " (" + ",".join(data.keys()) + ")"
+        
+        for k, v in data.iteritems():
+            if isinstance(v, (int, long, float)) :
+                v = self.db.escape_string(str(v))
+            else : 
+                v = '"' + self.db.escape_string(v.encode('utf8')) + '"'
+            data[k] = v
+            
+        query += " VALUES (" + ",".join(data.values()) + "); "
+        
+        if debug == True:
+            print query
+        
+        result = self.executeSql(query)
+        
+        return result
